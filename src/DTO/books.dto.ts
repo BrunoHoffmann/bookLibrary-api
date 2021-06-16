2d172f403884688eb4a2fd721a718daae246b45c
Bruno Hoffmann <brunohoffmanndev@gmail.com>
2021-06-15 22:25:00 -0300

    create author DTO

--- src/DTO/books.dto.ts
+++ src/DTO/books.dto.ts
@@ -1,5 +1,17 @@
-import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+  IsNumber,
+  IsPositive,
+  ArrayMinSize,
+  IsNotEmptyObject,
+  ValidateNested,
+} from 'class-validator';
+import { Type } from 'class-transformer';
 
+import { AuthorDTO } from './author.dto';
 export class BookDTO {
   @IsNotEmpty()
   @IsString()
@@ -8,14 +20,31 @@ export class BookDTO {
   readonly name: string;
 
   @IsNotEmpty()
-  readonly author: string[];
+  @Type(() => AuthorDTO)
+  @ArrayMinSize(1)
+  @IsNotEmptyObject({}, { each: true })
+  @ValidateNested({ each: true })
+  readonly author: AuthorDTO[];
 
   @IsNotEmpty()
+  @IsString()
+  @MinLength(2)
+  @MaxLength(100)
+  readonly language: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @IsPositive()
   readonly releaseYear: number;
 
   @IsNotEmpty()
+  @IsString()
+  @MinLength(2)
+  @MaxLength(100)
   readonly publisher: string;
 
   @IsNotEmpty()
+  @IsNumber()
+  @IsPositive()
   readonly pages: number;
 }
